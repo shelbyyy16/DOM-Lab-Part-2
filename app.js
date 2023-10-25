2750f2c2463816ecfa2c28a7172ef94c5f20dc68
honeybee <shelby.e.pagan@gmail.com>
2023-10-25 16:12:44 -0400

    formatting changes

--- app.js
+++ app.js
@@ -2,32 +2,22 @@ console.log("js:loaded");
 
 const menuLinks = [
   { text: "about", href: "/about" },
-  {
-    text: "catalog",
-    href: "#",
-    subLinks: [
+  {text: "catalog", href: "#",subLinks: [
       { text: "all", href: "/catalog/all" },
       { text: "top selling", href: "/catalog/top" },
       { text: "search", href: "/catalog/search" },
     ],
   },
-  {
-    text: "orders",
-    href: "#",
-    subLinks: [
+  {text: "orders", href: "#", subLinks: [
       { text: "new", href: "/orders/new" },
       { text: "pending", href: "/orders/pending" },
       { text: "history", href: "/orders/history" },
     ],
   },
-  {
-    text: "account",
-    href: "#",
-    subLinks: [
+  {text: "account", href: "#", subLinks: [
       { text: "profile", href: "/account/profile" },
       { text: "sign out", href: "/account/signout" },
-    ],
-  },
+    ]},
 ];
 
 //TASK 1
