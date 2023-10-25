a502fb84b7bf240d45720a985c4ca9ae51b72f0c
honeybee <shelby.e.pagan@gmail.com>
2023-10-25 14:36:53 -0400

    task 5 complete and running

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
-console.log('js:loaded');
+console.log("js:loaded");
 
 const menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '#', subLinks: [
-      {text: 'all', href: '/catalog/all'},
-      {text: 'top selling', href: '/catalog/top'},
-      {text: 'search', href: '/catalog/search'},
-    ]},
-    {text: 'orders', href: '#' , subLinks: [
-      {text: 'new', href: '/orders/new'},
-      {text: 'pending', href: '/orders/pending'},
-      {text: 'history', href: '/orders/history'},
-    ]},
-    {text: 'account', href: '#', subLinks: [
-      {text: 'profile', href: '/account/profile'},
-      {text: 'sign out', href: '/account/signout'},
-    ]},
-  ];
-
+  { text: "about", href: "/about" },
+  {
+    text: "catalog",
+    href: "#",
+    subLinks: [
+      { text: "all", href: "/catalog/all" },
+      { text: "top selling", href: "/catalog/top" },
+      { text: "search", href: "/catalog/search" },
+    ],
+  },
+  {
+    text: "orders",
+    href: "#",
+    subLinks: [
+      { text: "new", href: "/orders/new" },
+      { text: "pending", href: "/orders/pending" },
+      { text: "history", href: "/orders/history" },
+    ],
+  },
+  {
+    text: "account",
+    href: "#",
+    subLinks: [
+      { text: "profile", href: "/account/profile" },
+      { text: "sign out", href: "/account/signout" },
+    ],
+  },
+];
 
 //TASK 1
 
 const mainEl = document.querySelector("main");
 
-mainEl.style.backgroundColor = 'var(--main-bg)';
+mainEl.style.backgroundColor = "var(--main-bg)";
 
 mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
 
@@ -35,42 +46,38 @@ const topMenuEl = document.getElementById("top-menu");
 
 topMenuEl.style.height = "100%";
 
-topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
 
 topMenuEl.classList.add("flex-around");
 
 //TASK 3
 
 menuLinks.forEach(function (link) {
-    const anchorElement = document.createElement("a");
-    anchorElement.setAttribute("href", link.href);
-    anchorElement.textContent = link.text;
-    topMenuEl.appendChild(anchorElement);
+  const anchorElement = document.createElement("a");
+  anchorElement.setAttribute("href", link.href);
+  anchorElement.textContent = link.text;
+  topMenuEl.appendChild(anchorElement);
 });
-
 
 //TASK 4
 
-
-const subMenuEl = document.getElementById('sub-menu');
+const subMenuEl = document.getElementById("sub-menu");
 
 subMenuEl.style.height = "100%";
 
-subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)';
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
 
-subMenuEl.classList.add('flex-around');
+subMenuEl.classList.add("flex-around");
 
-subMenuEl.style.position = 'absolute';
+subMenuEl.style.position = "absolute";
 
-subMenuEl.style.top = '0';
+subMenuEl.style.top = "0";
 
 //TASK 5
 
-const topMenuLinks = topMenuEl.querySelectorAll('a');
+const topMenuLinks = topMenuEl.querySelectorAll("a");
 
 let showingSubMenu = false;
-
-// Assuming you have already selected topMenuEl, topMenuLinks, subMenuEl, and declared showingSubMenu as mentioned earlier.
 
 topMenuEl.addEventListener('click', function(event) {
     event.preventDefault();
@@ -78,14 +85,19 @@ topMenuEl.addEventListener('click', function(event) {
     if (event.target.tagName !== 'A') {
         return;
     }
-
+    topMenuLinks.forEach(function(link) {
+        link.classList.remove('active');
+    });
     if (event.target.classList.contains('active')) {
         event.target.classList.remove('active');
         showingSubMenu = false;
         subMenuEl.style.top = '0';
+        showingSubMenu = false;
         return;
     }
-    const linkObject = menuLinks.find(link => link.title === event.target.textContent);
+    event.target.classList.add('active');
+
+    const linkObject = menuLinks.find(link => link.text === event.target.textContent);
 
     if (linkObject && linkObject.subLinks) {
         showingSubMenu = true;
@@ -95,30 +107,21 @@ topMenuEl.addEventListener('click', function(event) {
         showingSubMenu = false;
         subMenuEl.style.top = '0';
     }
-    
-    event.target.classList.add('active');
-
     console.log(event.target.textContent);
 
-    topMenuLinks.forEach(function(link) {
-        link.classList.remove('active');
-    });
-
-    if (event.target.textContent === 'About') {
+    if (event.target.textContent === 'about') {
         mainEl.innerHTML = '<h1>about</h1>';
     }
 });
 
+
 function buildSubMenu(subLinks) {
-    
-    subMenuEl.innerHTML = '';
+  subMenuEl.innerHTML = "";
 
-    
-    subLinks.forEach(function(link) {
-        const subLinkElement = document.createElement('a');
-        subLinkElement.href = link.href; 
-        subLinkElement.textContent = link.text; 
-        subMenuEl.appendChild(subLinkElement); 
-    });
+  subLinks.forEach(function (link) {
+    const subLinkElement = document.createElement("a");
+    subLinkElement.href = link.href;
+    subLinkElement.textContent = link.text;
+    subMenuEl.appendChild(subLinkElement);
+  });
 }
-
